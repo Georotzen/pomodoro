4e00870f9248d6a4908afbf7098695e11a2180f5
Brandon Eichler <beedat1@gmail.com>
2016-01-09 23:30:55 -0600

    basic html and css finished, still needs javascript

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -1,3 +1,11 @@
-$(document).ready(function(){
-
+$(document).ready(function() {
+    var per = 0;
+    setInterval(function() {
+        per++;
+        if (per <= 100) {
+            $('.clock-container').css({
+                background: "linear-gradient(to top, green " + per + "%,transparent " + per + "%,transparent 100%)"
+            });
+        }
+    }, 100);
 });
