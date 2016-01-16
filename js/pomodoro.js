ae1e8dbe58fa1e4969bc4df068448ea58b79b3b7
Brandon Eichler <beedat1@gmail.com>
2016-01-16 11:55:59 -0600

    added code comments

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -1,14 +1,17 @@
 // Global Variables
-var $clockDisplay = $(".clock-display"),
-    $clockContainer = $(".clock-container"),
-    $clockContainerBefore = $(".clock-container-before"),
-    sessionLength = $(".session > .length").text() * 60,
+var sessionLength = $(".session > .length").text() * 60,
     breakLength = $(".break > .length").text() * 60,
     sessionColor = "#2F8C2F",
     breakColor = "#AF3A3A",
     sound = true,
     t;
 
+// Global cached jQuery Selectors
+var $clockDisplay = $(".clock-display"),
+    $clockContainer = $(".clock-container"),
+    $clockContainerBefore = $(".clock-container-before");
+
+// Minute or MinuteS
 function pluralize(amount) {
     var grammar;
     if (amount > 1) grammar = "minutes";
@@ -16,17 +19,24 @@ function pluralize(amount) {
     return grammar;
 }
 
+/* Updates the Break and Session Length displays
+	btn will be "plus" OR "minus"
+	btnParent will be ".session" OR ".break"
+*/
 function updateLength(btn, btnParent) {
+    // cache the jQuery selectors we will use
     var $thisLength = $(btnParent).find($(".length")),
-        $thisTimeUnit = $(btnParent).find($(".time-unit")),
-        thisTime = $thisLength.text();
+        $thisTimeUnit = $(btnParent).find($(".time-unit"));
+
+    // Get the time length that is currently shown
+    var thisTime = $thisLength.text();
 
     // Adjust the option time length display
     if (btn === "plus" && thisTime < 1000) thisTime++;
     else if (btn === "minus" && thisTime > 1) thisTime--;
 
-    // Adjust the main clock display if the btnParent is "Session Length" and clock is not running
-    if (btnParent === ".session" && !t.isRunning()) {
+    // Update the clock display if the clock isn't active
+    if (btnParent === ".session" && !t.isActive()) {
         $clockDisplay.html(thisTime + "<br><span>" + pluralize(thisTime) + "</span>");
         $(".clock-which").text("Session");
     }
@@ -35,38 +45,38 @@ function updateLength(btn, btnParent) {
     $thisLength.text(thisTime);
     $thisTimeUnit.text(pluralize(thisTime));
 
-    // Update the time variables and current clock time
+    // Update the time variables and current clock time if the clock isn't active
     if (btnParent === ".session") {
         sessionLength = thisTime * 60;
-        if (t.isRunning()) t.update(btnParent, sessionLength);
+        if (t.isActive()) t.update(btnParent, sessionLength);
     } else if (btnParent === ".break") {
         breakLength = thisTime * 60;
-        if (t.isRunning()) t.update(btnParent, breakLength);
+        if (t.isActive()) t.update(btnParent, breakLength);
     }
 
     return;
 }
 
+// Timer object
 var timer = function(sTime, bTime, display) {
     var hours,
         minutes,
         seconds,
         intervalID,
-        that = this,
         paused = false,
-        running = false,
+        active = false,
         startSecs = sTime,
         counter = sTime + 1,
         color = sessionColor,
         whichActive = ".session";
 
-
     var startTimer = function(which) {
         if (!paused) {
             counter--;
-            updateDisplay(which);
-            // switch from Session to Break and vice versa when the timer has 0 seconds remaining
+            updateDisplay(which); // update the clock display in hours/minutes/seconds
+            // start playing sound with 1 second remaining if sound is on
             if (counter == 1 && sound) $(".alarm").trigger("play");
+            // switch from Session to Break and vice versa when the timer has 0 seconds remaining
             if (counter < 1) switchTimer(which);
         }
     }
@@ -76,6 +86,7 @@ var timer = function(sTime, bTime, display) {
         minutes = ((counter % 3600) / 60) | 0;
         seconds = (counter % 60) | 0;
 
+        // Pad the time with zeros if needed
         hours = hours < 10 ? "0" + hours : hours;
         minutes = minutes < 10 ? "0" + minutes : minutes;
         seconds = seconds < 10 ? "0" + seconds : seconds;
@@ -86,6 +97,7 @@ var timer = function(sTime, bTime, display) {
         return;
     }
 
+    // gradually fills the clock background color based on seconds remaining
     var fill = function() {
         var per = Math.abs((counter / startSecs) * 100 - 100);
         per++;
@@ -94,6 +106,7 @@ var timer = function(sTime, bTime, display) {
         });
     }
 
+    // change from Session to Break and vice versa
     var switchTimer = function(which) {
         clearInterval(intervalID);
         var w = which;
@@ -117,14 +130,17 @@ var timer = function(sTime, bTime, display) {
         }, 1000);
     }
 
+    // Turns the sound on and off
     this.toggleSound = function() {
         if (sound) sound = false;
         else sound = true;
     }
 
+    // If there is no active timer we need to make and start a new interval
+    // if there is already an active timer, then just un-pause it so it continues running
     this.run = function() {
-        if (!running) {
-            running = true;
+        if (!active) {
+            active = true;
             paused = false;
             startTimer("Session");
             $clockContainerBefore.css("border-color", color);
@@ -136,28 +152,33 @@ var timer = function(sTime, bTime, display) {
         }
     }
 
+    // Clears the current active timer
     this.restart = function() {
-        running = false;
+        active = false;
         paused = true;
         $clockContainer.css("background", "transparent");
         $clockContainerBefore.css("border-color", "#FFF");
         return clearInterval(intervalID);
     }
 
+    // Pause the timer
     this.pause = function() {
         return paused = true;
     }
 
-    this.isRunning = function() {
-        return running;
+    // Let's us know if the timer is already active (paused or running)
+    this.isActive = function() {
+        return active;
     }
 
+    // updates the Session and Break lengths
     this.update = function(which, newTime) {
         if (which === ".session") return sTime = newTime;
         else if (which === ".break") return bTime = newTime;
     }
 }
 
+// Listens for buttons/divs to be clicked and perform specific actions
 function buttonListeners() {
     $(".minus, .plus").click(function() {
         var btnParent = "." + $(this).parent().attr("class"),
@@ -167,7 +188,7 @@ function buttonListeners() {
     });
 
     $(".start").click(function() {
-        if (!t.isRunning()) t = new timer(sessionLength, breakLength, $clockDisplay);
+        if (!t.isActive()) t = new timer(sessionLength, breakLength, $clockDisplay);
         $(this).hide(0, function() {
             $(".pause").show(0);
         });
@@ -182,7 +203,7 @@ function buttonListeners() {
     });
 
     $(".reset").click(function() {
-        if (t.isRunning()) {
+        if (t.isActive()) {
             $(".pause").hide(0, function() {
                 $(".start").show(0);
             });
